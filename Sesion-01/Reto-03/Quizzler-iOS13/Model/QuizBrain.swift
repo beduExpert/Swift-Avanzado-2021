c1a054fd813922563deac175c31bb76360cbb2c1
Arlen <arlen.pena@gmail.com>
2021-10-20 16:43:19 -0500

    actualización del reto 03

---
+++ Sesion-01/Reto-03/Quizzler-iOS13/Model/QuizBrain.swift
@@ -0,0 +1,17 @@
+
+import Foundation
+
+struct QuizBrain {
+    
+    var questionNumber = 0
+    var score = 0
+    
+    let quiz = [
+        Question(q: "Which is the largest organ in the human body?", a: ["Heart", "Skin", "Large Intestine"], correctAnswer: "Skin"),
+        Question(q: "Five dollars is worth how many nickels?", a: ["25", "50", "100"], correctAnswer: "100")
+    ]
+    
+ 
+    
+}
+
